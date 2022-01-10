f4f638430b692976f85681cc347978415c4beea9
Gorelko90 <gorelko90@gmail.com>
2022-01-10 16:49:20 +0300

    Commit 4

--- collection/src/test/java/ownLinkedList/OwnLinkedListTest.java
+++ collection/src/test/java/ownLinkedList/OwnLinkedListTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 class OwnLinkedListTest {
-
     @Test
     void add() {
         OwnLinkedList<String> ownList = new OwnLinkedList<>();
