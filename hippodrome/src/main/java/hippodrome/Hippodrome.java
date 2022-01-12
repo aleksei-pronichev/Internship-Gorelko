a8c53f9207fdd2e912e0669abfcc8d0b8d2dc4a0
Gorelko90 <gorelko90@gmail.com>
2022-01-13 00:17:35 +0300

    Commit 1

---
+++ hippodrome/src/main/java/hippodrome/Hippodrome.java
@@ -0,0 +1,62 @@
+package hippodrome;
+
+import java.util.ArrayList;
+
+public class Hippodrome {
+
+    private final ArrayList<Horse> horses = new ArrayList<>();
+
+    public ArrayList<Horse> getHorses() {
+        return horses;
+    }
+
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            move();
+            try {
+                Thread.sleep(4);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void move() {
+        for (Horse horse : horses) {
+            horse.move();
+        }
+    }
+
+    public Horse getWinner() {
+        double max = 0;
+        Horse winner = null;
+        for (Horse horse : horses) {
+            if (horse.getDistance() > max) {
+                max = horse.getDistance();
+                winner = horse;
+            }
+        }
+        return winner;
+    }
+
+    public void printWinner() {
+        Horse winner = getWinner();
+        System.out.println("Winner is " + winner.getName() + "!");
+    }
+
+    public static Hippodrome game;
+
+    public static void main(String[] args) {
+        game = new Hippodrome();
+        Horse horse1 = new Horse("h1", 3, 0);
+        Horse horse2 = new Horse("h2", 3, 0);
+        Horse horse3 = new Horse("h3", 3, 0);
+
+        game.getHorses().add(horse1);
+        game.getHorses().add(horse2);
+        game.getHorses().add(horse3);
+
+        game.run();
+        game.printWinner();
+    }
+}
