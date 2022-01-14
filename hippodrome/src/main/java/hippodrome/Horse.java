a87881f5777eeb45af40743885df4a1e21afcb64
Gorelko90 <gorelko90@gmail.com>
2022-01-14 09:57:02 +0300

    Commit hippodrome 1

---
+++ hippodrome/src/main/java/hippodrome/Horse.java
@@ -0,0 +1,26 @@
+package hippodrome;
+
+public class Horse {
+    private String name;
+    private double speed;
+    private double distance;
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void move() {
+        distance += speed * Math.random();
+    }
+
+}
