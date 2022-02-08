89a0152063770e91f924631c69b7846751385fdd
Gorelko90 <gorelko90@gmail.com>
2022-02-08 13:16:10 +0300

    Commit flyway 1

---
+++ flyway/src/main/resources/db/migration/V2__inset_into_table_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(username, first_name, last_name)
+VALUES ('test1', 'afriano', 'ps');
+INSERT INTO users(username, first_name, last_name)
+VALUES ('flywaytest', 'agus', 'bapak');
---
+++ flyway/target/classes/db/migration/V2__inset_into_table_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(username, first_name, last_name) VALUES('test1', 'afriano', 'ps');
+INSERT INTO users(username, first_name, last_name) VALUES('flywaytest', 'agus', 'bapak');
